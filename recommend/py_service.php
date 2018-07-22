d556b6efa522ca65d856d96e716627e0e9e9172d
peng.chengcheng <peng.chengcheng@bestv.com.cn>
2018-07-22 12:11:42 +0800

    add python script call

---
+++ recommend/py_service.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pcc
+ * Date: 2018/7/11
+ * Time: 18:52
+ */
+//$DEFAULT_PYTHON_FILE = './exec_py.py';
+$DEFAULT_PYTHON_FILE = '../../ec_eclipse/dps_predict.py';  //path to python scirpts
+
+function exec_python($pyFileName){
+    $PYTHON_HOME = "/usr/local/bin/";
+    $pre_cmd = $PYTHON_HOME . "python3 " . $pyFileName;
+//    $pre_cmd = "/usr/local/bin/python3 /Users/pcc/ec_eclipse/dps_predict.py";
+//    $pre_cmd = "/usr/local/bin/python3 " . $pyFileName;
+//    echo $pre_cmd;
+    $cmd = exec($pre_cmd,$cmd,$ret);
+//    $cmd = system($pre_cmd,$ret);
+//    echo("ret is $ret  ");
+
+    return array('cmd' => $cmd, 'ret' => $ret);
+}
+
+
+function results_jsonEncode($resArray){
+    $api_output = array(
+        'data'=>array(),
+        'message'=>'',
+        'code'=>2
+    );
+    if($resArray['ret'] != 0){
+        $api_output['message'] = '<span style="color:red;">' . $resArray['cmd'] .'</span>';
+        $api_output['code'] = $resArray['ret'];
+    }
+    else{
+        $api_output['data'] = $resArray['cmd'];
+        $api_output['message'] = '';
+        $api_output['code'] = $resArray['ret'];
+    }
+    $json_print = json_encode($api_output, JSON_PRETTY_PRINT);
+    return $json_print;
+
+}
+
+if(isset($_POST['checkPython'])){
+    getPyResults($DEFAULT_PYTHON_FILE);
+    exit();
+}
+
+function getPyResults($py_file){
+    $res = exec_python($py_file);
+    echo results_jsonEncode($res);
+}
+
+echo getPyResults($DEFAULT_PYTHON_FILE);
+//echo exec_python($DEFAULT_PYTHON_FILE);
+
+?>
+
---
+++ tetst_py/test_py.js
@@ -0,0 +1,49 @@
+/**
+ * Created by pcc on 2018/7/11.
+ */
+
+
+//document.write("<script type='text/javascript' src='../jquery-3.3.1.min.js'></script>");
+//document.write("<script type='text/javascript' src='../jq_plugins.js'></script>");
+
+
+$(document).ready(function(){
+    checkPythonResults();
+
+});
+
+
+function checkPythonResults(){
+    var execRes = "";
+    $('#python-result').empty();
+
+    $.ajax(
+        {
+            type:'post',
+            url:'py_service.php',
+            data:{
+                checkPython:true
+            },
+            dataType:'json',
+            success:function(result){
+                if(result["code"]!=0){
+                    //If the results of query is abnormal, display error msg given by backend.
+                    execRes = result["message"];
+                }
+                else{
+                    // If the response data is iterable, use EACH for iteration
+                    //$.each(result["data"],function(i,n){
+                    //    //var tarPrice 	= n["pred_price"];
+                    //    execRes+= 	"<p>" + n + "</p>";
+                    //    //$execRes+= 	n;
+                    //
+                    //});
+                    execRes+= 	"<p>" + result["data"] + "</p>";  //If the data is not iterable, just append
+                }
+
+                $('#python-result').empty();
+                $('#python-result').append(execRes);
+            }
+        }
+    )
+}
